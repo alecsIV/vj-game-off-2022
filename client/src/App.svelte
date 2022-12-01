265299dd794c58ac2f569e864ee801a0fe856c55
Matthew Taylor <matthew.taylor03@bbc.co.uk>
2022-12-01 11:35:09 +0000

    [wip]: setup frontend

---
+++ client/src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { Router, Route, Link } from './../node_modules/svelte-navigator/';
+  import NewGame from './lib/NewGame.svelte';
+</script>
+
+<Router>
+
+  <main>
+    <Route path="/">
+      <h1>Back to the Drawing Board</h1>
+      <!-- <Link to="lobby">Create a game</Link> -->
+      <NewGame />
+      <Link to="rules">How to play</Link>
+    </Route>
+
+    <Route path="rules">
+      <Link to="/">Back</Link>
+    </Route>
+
+    <Route path="lobby">
+      <form>
+        <label for="nickname"></label>
+        <input id="nickname" type="text" />
+      </form>
+    </Route>
+  </main>
+  
+  <style>
+    .logo {
+      height: 6em;
+      padding: 1.5em;
+      will-change: filter;
+    }
+    .logo:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    .logo.svelte:hover {
+      filter: drop-shadow(0 0 2em #ff3e00aa);
+    }
+    .read-the-docs {
+      color: #888;
+    }
+  </style>
+</Router>
