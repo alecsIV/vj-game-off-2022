4ff599dd7ef69e76d851b9c8bf61e571cdcd3882
Matthew Taylor <matthew.taylor03@bbc.co.uk>
2022-12-01 11:34:15 +0000

    [wip] frontend
    Co-authored-by: Alexander Ivanov <alecsIV@users.noreply.github.com>
    Co-authored-by: Alexandra Nicolaides <alexandra257@users.noreply.github.com>

---
+++ client/src/lib/NewGame.svelte
@@ -0,0 +1,24 @@
+<script>
+  function createGame() {
+    const id = generateId(6);
+    window.location.href += `lobby#${id}`;
+  }
+
+    // dec2hex :: Integer -> String
+    // i.e. 0-255 -> '00'-'ff'
+    function dec2hex(dec) {
+      return dec.toString(16).padStart(2, "0");
+    }
+
+    // generateId :: Integer -> String
+    function generateId(len) {
+      var arr = new Uint8Array((len || 40) / 2);
+      window.crypto.getRandomValues(arr);
+      return Array.from(arr, dec2hex).join("");
+    }
+
+</script>
+
+<button class="new-game__button" on:click={createGame}
+  >> Create a new game
+</button>
