db78d0aba3e1ce4154a8977d2349b434caae002e
Alexander Ivanov <36383919+alecsIV@users.noreply.github.com>
2022-12-05 12:17:49 +0000

    List users in separate rooms

--- client/src/lib/Lobby.svelte
+++ client/src/lib/Lobby.svelte
@@ -1,17 +1,20 @@
 <script>
   export let socket;
+  let nickname;
   const submitForm = () => {
     const id = window.location.hash.replace('#', '');
-    console.log('id', id);
-    socket.emit("join-room", id, response => {
-      const { content } = response;
+    console.log('socket', socket);
+    socket.emit("join-room", id, nickname, response => {
+      const { content, users, numUsers } = response;
       console.log(content);
+      console.log('Logged users', users);
+      console.log('Total users', numUsers);
     });
   };
 </script>
 
 <form on:submit|preventDefault={submitForm}>
   <label for="nickname" />
-  <input id="nickname" type="text" />
+  <input id="nickname" type="text" bind:value={nickname}/>
   <button type="submit">Submit</button>
 </form>
--- server.js
+++ server.js
@@ -2,39 +2,40 @@ import { readFileSync } from "fs";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-const httpServer = createServer((req, res) => {
-  if (req.url !== "/") {
-    res.writeHead(404);
-    res.end("Not found");
+const httpServer = createServer( ( req, res ) => {
+  if ( req.url !== "/" ) {
+    res.writeHead( 404 );
+    res.end( "Not found" );
     return;
   }
   // reload the file every time
-  const content = readFileSync("./index.html");
-  const length = Buffer.byteLength(content);
+  const content = readFileSync( "./index.html" );
+  const length = Buffer.byteLength( content );
 
-  res.writeHead(200, {
+  res.writeHead( 200, {
     "Content-Type": "text/html",
     "Content-Length": length,
-  });
-  res.end(content);
-});
+  } );
+  res.end( content );
+} );
 
-const io = new Server(httpServer, {
-    // Socket.IO options
-    cors: {
-        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
-        credentials: true
-    },
-});
+const io = new Server( httpServer, {
+  // Socket.IO options
+  cors: {
+    origin: [ 'http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://localhost:3000' ],
+    credentials: true,
+    methods: [ "GET", "POST" ],
+  },
+} );
 
 let users = {};
 
-io.on("connection", (socket) => {
-  console.log(`connect ${socket.id}`);
+io.on( "connection", ( socket ) => {
+  console.log( `connect ${socket.id}` );
 
-  socket.on("disconnect", (reason) => {
-    console.log(`disconnect ${socket.id} due to ${reason}`);
-  });
+  socket.on( "disconnect", ( reason ) => {
+    console.log( `disconnect ${socket.id} due to ${reason}` );
+  } );
 
   /* @TODO:
   - show number of connected users
@@ -42,11 +43,30 @@ io.on("connection", (socket) => {
   */
   // socket.join('default room');
 
-  socket.on('join-room', (room, callback) => {
-    socket.join(room);
-    callback({
-      content: `Joined ${room}`,
-    });
+  socket.on( 'join-room', ( room, nickname, callback ) => {
+    let err;
+    if ( !users[ room ] ) users[ room ] = new Set();
+    if ( users[ room ].has( nickname ) ) {
+      err = 'User already exists';
+      callback( {
+        content: err,
+        success: false,
+        users: Array.from(users[room]),
+        numUsers: users[ room ].size
+      } )
+      return;
+    }
+
+    users[ room ].add( nickname );
+
+    socket.join( room );
+
+    callback( {
+      content: `${nickname} joined ${room}`,
+      success: true,
+      users: Array.from(users[room]),
+      numUsers: users[ room ].size
+    } );
     // socket.to('game1').emit('chat message', 'game 1');
     // console.log('joined room game 1');
     // io.to('game1').emit('chat-message');
@@ -55,12 +75,12 @@ io.on("connection", (socket) => {
     //     id: socket.id,
     // });
 
-  //   socket.on('send-nickname', (nickname) => {
-  //     socket.nickname = nickname;
-  //     users.push(socket.nickname);
-  //     console.log(users);
-  // });
-})
-});
+    //   socket.on('send-nickname', (nickname) => {
+    //     socket.nickname = nickname;
+    //     users.push(socket.nickname);
+    //     console.log(users);
+    // });
+  } )
+} );
 
-httpServer.listen(3000);
+httpServer.listen( 3000 );
